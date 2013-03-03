36107f3d7e5e798de92483fb1544bbea7a269726
Atul <atul@atul.(none)>
2013-03-03 15:14:46 +0530

    output of bash script

--- dirlisting.sh
+++ dirlisting.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 dir=~/home/atul
+cd $dir
 for d in $dir; do #for all subdir in dir 
 	cd $d	#enter the subdir
 	ls
 done
 cd ..	#return to previous directory
+
+#OUTPUT OF ABOVE SCRIPT
+#atul@atul:~/Desktop/Hello-world$ ./dirlisting.sh
+#./dirlisting.sh: line 3: cd: /home/atul/home/atul: No such file or directory
+#./dirlisting.sh: line 5: cd: /home/atul/home/atul: No such file or directory
 
 	
 
