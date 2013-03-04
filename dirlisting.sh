25f549a80c43c5a2afcb828fae37572fafcd684a
Atul <amkcat9@gmail.com>
2013-03-04 10:40:48 +0530

    changed path

--- dirlisting.sh
+++ dirlisting.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-dir=("/home/mangesh"
-	 "/home/mangesh/Documents"
-	 "/home/mangesh/Downloads")
+dir=("/home/atul"
+	 "/home/atul/Documents"
+	 "/home/atul/Downloads")
 echo "listing sub directories of" ${#dir[@]} "parent directories"
 for d in "${dir[@]}"; do #for each directory in dir 
 	echo "listing" ${d}
