531d2b52f4ae7453845dc1edd9d0a7cdc55bc8fc
Atul <amkcat9@gmail.com>
2013-03-04 12:35:47 +0530

    declare an array dir that stores listing of /home/atul/*

--- dirlisting.sh
+++ dirlisting.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-dir=("/home/atul"
-	 "/home/atul/Documents"
-	 "/home/atul/Downloads")
+dir=(`ls -d /home/atul/*`)
 echo "listing sub directories of" ${#dir[@]} "parent directories"
 for d in "${dir[@]}"; do #for each directory in dir 
 	echo "listing" ${d}
