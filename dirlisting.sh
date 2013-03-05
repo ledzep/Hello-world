dd880d2648a7da6f72e2c8ee25065071dd707ff2
Atul <amkcat9@gmail.com>
2013-03-05 13:47:56 +0530

    git pull script

--- dirlisting.sh
+++ dirlisting.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-dir=(`ls -d /home/atul/*`)
-echo "listing sub directories of" ${#dir[@]} "parent directories"
-for d in "${dir[@]}"; do #for each directory in dir 
-	echo "listing" ${d}
-	cd "${d}"	#enter the dir
-	ls
+PROJ="/home/mangesh/webserver_test"
+
+for d in `ls -d $PROJ/*`; do
+	cd "$d"
+	#echo "=== print git status of $d"
+	git status >/dev/null 2>/dev/null
+	GIT_ERR="$?"
+	if [ "$GIT_ERR" == "0" ]; then
+		echo "valid git repo $d"
+		git pull origin master
+	else
+		echo "Error: invalid git repo $d. Error-code: $GIT_ERR"
+	fi 
+	cd "$PROJ" 
 done
-
-
-	
-
-
-
 
 
 
