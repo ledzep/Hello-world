afded74d587e75e6d171cf65c50e173b1d7b5fb1
Atul <amkcat9@gmail.com>
2013-03-07 14:54:20 +0530

    SQL added

---
+++ publication.sql
@@ -0,0 +1,82 @@
+-- MySQL dump 10.13  Distrib 5.1.67, for debian-linux-gnu (i686)
+--
+-- Host: 127.0.0.1    Database: publications
+-- ------------------------------------------------------
+-- Server version	5.5.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `classics`
+--
+
+DROP TABLE IF EXISTS `classics`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `classics` (
+  `author` varchar(128) DEFAULT NULL,
+  `title` varchar(128) DEFAULT NULL,
+  `category` varchar(16) DEFAULT NULL,
+  `year` smallint(6) DEFAULT NULL,
+  `isbn` char(13) NOT NULL DEFAULT '',
+  PRIMARY KEY (`isbn`),
+  KEY `author` (`author`(20)),
+  KEY `title` (`title`(20)),
+  KEY `category` (`category`(4)),
+  KEY `year` (`year`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `classics`
+--
+
+LOCK TABLES `classics` WRITE;
+/*!40000 ALTER TABLE `classics` DISABLE KEYS */;
+INSERT INTO `classics` VALUES ('Mark Twain','The Adventures of Tom Sawyer','Fiction',1876,'9781598184891'),('Jane Austen','Pride and Prejudice','Fiction',1811,'9780582506206'),('Charles Darwin','The orgin of Species','Non-Fiction',1856,'9780517123201'),('Charles Dicken','The Old Curiosity Shop','Fiction',1841,'9780099533474'),('William Shakespeare','Romeo and Juliet','Play',1594,'9780192814968'),('Jeffery Archer','Quiver full of arrows','Fiction',1997,'0091243876231');
+/*!40000 ALTER TABLE `classics` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customers` (
+  `name` varchar(128) DEFAULT NULL,
+  `isbn` varchar(128) NOT NULL DEFAULT '',
+  PRIMARY KEY (`isbn`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES ('Joe Bloggs','9780099533474'),('Mary Smith','9780582506206'),('Jack Wilson','9780517123201');
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-03-07 14:51:43
